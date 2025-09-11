2f479eb3f5bf528945b8835b2c75dfb3b8ce27ee
BindushreeGD <bindugd2001@gmail.com>
2025-09-11 10:09:21 +0530

    updated1

--- backend/app.py
+++ backend/app.py
@@ -7,8 +7,4 @@ def home():
     return jsonify({"message": "Hello from Flask backend!"})
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     app.run(host="0.0.0.0", port=8000)
-=======
-    app.run(host="0.0.0.0", port=8000)
->>>>>>> 9027baa7a88aaff63f4fc2a2d04faff5f972836f
